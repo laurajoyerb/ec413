3cfe486bb064c4f7564b554af290b0fbfea65b95
Laura Joy Erb <laurajoy@bu.edu>
2019-02-10 22:59:43 -0500

    finished problem 5

--- lab0/prob5.c
+++ lab0/prob5.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// accepts an integer a, and prints it out in decimal.
+int comp (const void * elem1, const void * elem2)
+{
+    int f = *((int*)elem1);
+    int s = *((int*)elem2);
+    if (f > s) return  1;
+    if (f < s) return -1;
+    return 0;
+}
+
+// Global variables for binary search (easier for recursion)
+int first, middle, last, search, ind;
+int arr[1000];
+
+void binSearch(int first, int middle, int last, int search, int arr[])
+{
+  if (first > last)
+  {
+    printf("Number not found\n");
+    return;
+  }
+  else
+  {
+    if (arr[middle] < search)
+    {
+      first = middle + 1;
+      middle = (first + last)/2;
+      return binSearch(first, middle, last, search, arr);
+    }
+    else if (arr[middle] == search)
+    {
+      ind = middle;
+    }
+    else
+    {
+      last = middle - 1;
+      middle = (first + last)/2;
+      return binSearch(first, middle, last, search, arr);
+    }
+  }
+}
+
+// accepts an array of user-specified size as well as a desired element--the program returns the index of this element in the sorted array
 int main() {
     int size;
     printf("Please enter the size of the array, each element of the array, and then the desired index of the sorted array:\n");
-    scanf("%d", &size);
+    scanf("%d", &size); // holds the size of the array
 
-    int* arr = malloc(size*sizeof(int)); // arr points to head of array
-    int* scanner = arr; // extra pointer to go through array
+    int* arrP = malloc(size*sizeof(int)); // arr points to head of array
+    int* scanner = arrP; // extra pointer to go through array
 
     for (int i = 0; i < size; i++)
     {
@@ -16,10 +57,23 @@ int main() {
       scanner++;
     }
 
-    int search; // holds index to be returned after sorting
+    // holds element to be searched after sorting
     scanf("%d", &search);
 
-    
+    // populates array without pointer weirdness
+    for (int i = 0; i < size; i++)
+    {
+      arr[i] = *arrP;
+      arrP++;
+    }
 
-    printf("\n");
+    qsort(arr, size, sizeof(*arr), comp); // sorts array
+
+    first = 0;
+    last = size - 1;
+    middle = (first + last)/2;
+
+    binSearch(first, middle, last, search, arr);
+
+    printf("%d\n", ind); // prints index of desired element
 }
